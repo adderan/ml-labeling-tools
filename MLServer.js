0f6929b4b7c7ae4d688cc196999e52d11c319f6e
Alden Deran <aldenderan@protonmail.com>
2024-05-15 00:53:44 -0700

    Add page for viewing metrics

--- MLServer.js
+++ MLServer.js
@@ -2,6 +2,95 @@ import {idb} from "./infinitydb/src/javascript/infinitydb_access.js";
 
 const INTERFACE = "com.infinitydb.ai";
 
+export class ServerLoginPane {
+    constructor(server) {
+        this.server = server;
+        this.root_div = document.createElement('div');
+        this.root_div.classList.add('modal');
+
+        this.content_div = document.createElement('div');
+        this.content_div.classList.add('modal-content', 'animate');
+        this.root_div.appendChild(this.content_div);
+
+        let server_label = document.createElement("label");
+        server_label.textContent = "Server:";
+        this.server_field = document.createElement('input');
+        this.server_field.classList.add('modal-input');
+        server_label.appendChild(this.server_field);
+        this.content_div.appendChild(server_label);
+
+        let username_label = document.createElement('label');
+        username_label.textContent = "Username:";
+        this.username_field = document.createElement('input');
+        this.username_field.classList.add('modal-input');
+        username_label.appendChild(this.username_field);
+        this.content_div.appendChild(username_label);
+
+        let password_label = document.createElement('label');
+        password_label.textContent = "Password:";
+        this.password_field = document.createElement('input');
+        this.password_field.classList.add('modal-input');
+        password_label.appendChild(this.password_field);
+        this.content_div.appendChild(password_label);
+
+        
+        let button_area = document.createElement('div');
+        button_area.classList.add('modal-button-area');
+        this.connect_button = document.createElement('button');
+        this.connect_button.textContent = "Connect";
+        this.connect_button.classList.add('modal-button');
+
+        this.connect_button.onclick = (event) => {
+            this.save_credentials();
+            this.hide();
+        };
+        button_area.appendChild(this.connect_button);
+
+        this.cancel_button = document.createElement('button');
+        this.cancel_button.textContent = "Cancel";
+        this.cancel_button.classList.add('modal-button', 'cancelbtn');
+        this.cancel_button.onclick = (event) => {
+            this.hide();
+
+        };
+        button_area.appendChild(this.cancel_button);
+
+        this.content_div.appendChild(button_area);
+
+        document.body.appendChild(this.root_div);
+
+        this.load_credentials();
+
+    }
+    save_credentials() {
+        let server_url = this.server_field.value;
+        let db = "ai/labels";
+        let username = this.username_field.value;
+        let password = this.password_field.value;
+
+        sessionStorage.setItem('server_url', server_url);
+        sessionStorage.setItem('db', db);
+        sessionStorage.setItem('username', username);
+        sessionStorage.setItem('password', password);
+        this.server.set_credentials(server_url, db, username, password);
+    }
+    load_credentials() {
+        let server_url = sessionStorage.getItem('server_url');
+        let db = sessionStorage.getItem('db');
+        let username = sessionStorage.getItem('username');
+        let password = sessionStorage.getItem('password');
+        this.server_field.value = server_url;
+        this.username_field.value = username;
+        this.password_field.value = password;
+        this.server.set_credentials(server_url, db, username, password);
+    }
+    show() {
+        this.root_div.style.display = 'block';
+    }
+    hide() {
+        this.root_div.style.display = 'none';
+    }
+}
 export class MLServer extends idb.Accessor {
     constructor(server_url, db, username, password) {
         super(server_url, db, username, password);
@@ -25,6 +114,35 @@ export class MLServer extends idb.Accessor {
         );
         let label_sets = idb.flattenToLists(result["_label_set"]);
         return label_sets;
+    }
+    async getModels() {
+        let [success, result, content_type] = await this.execute_query(
+            [INTERFACE, "get_model_names"]
+        );
+        //let model_names = idb.flattenToLists(result);
+        let model_names = Object.keys(result["_model_id"]);
+
+        return model_names;
+    }
+    async getModelMetrics(model_id) {
+        let [success, result, content_type] = await this.execute_query(
+            [INTERFACE, "get_model_metrics"],
+            {
+                "_model_id": model_id
+            }
+        );
+
+        let metrics = {};
+        for (let [epoch, epoch_metrics] of Object.entries(result["_Epoch"])) {
+            epoch = idb.unQuote(epoch);
+            epoch_metrics = epoch_metrics["_metrics"];
+            let epoch_metrics_unquoted = {};
+            for (let [metric_name, metric] of Object.entries(epoch_metrics)) {
+                epoch_metrics_unquoted[idb.unQuote(metric_name)] = metric;
+            }
+            metrics[epoch] = epoch_metrics_unquoted;
+        }
+        return metrics;
     }
     async setLabel(label_set, image_set, image_id, label) {
         //image_id = idb.unflattenFromLists(image_id.slice(1));
---
+++ models.js
@@ -0,0 +1,79 @@
+import {MLServer, ServerLoginPane} from "./MLServer.js";
+import 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.js';
+
+
+
+var server = new MLServer(null, null, null, null);
+var login_pane = new ServerLoginPane(server)
+
+
+let login_button = document.getElementById('login');
+login_button.onclick = (event) => {
+    login_pane.show();
+
+};
+
+//// ** @type {HTMLCanvasElement} */
+let canvas = document.getElementById("chart");
+let chart = null;
+
+
+
+////** @type {HTMLSelectElement} */
+let model_select_box = document.getElementById('model-select');
+let model_ids = await server.getModels();
+model_ids.map((model_id) => model_select_box.options.add(new Option(model_id, model_id)));
+if (sessionStorage.getItem("selected_model")) {
+    model_select_box.selectedIndex = sessionStorage.getItem("selected_model");
+
+}
+model_select_box.onchange = (event) => {
+    plot_metrics();
+    sessionStorage.setItem("selected_model", model_select_box.selectedIndex);
+
+};
+
+Chart.defaults.font.size = 20;
+model_select_box.dispatchEvent(new Event('change'));
+
+async function plot_metrics() {
+    let model_id = model_ids[model_select_box.selectedIndex];
+    let metrics = await server.getModelMetrics(model_id);
+    console.log(metrics);
+
+    let datasets = [];
+    for (let metric_name of Object.keys(metrics[0])) {
+        let dataset = {};
+        dataset.label = metric_name;
+        dataset.data = Object.keys(metrics).map((epoch) => metrics[epoch][metric_name]);
+        dataset.borderWidth = 4;
+        console.log(dataset);
+        datasets.push(dataset);
+    }
+    if (chart) {
+        chart.destroy();
+    }
+    chart = new Chart(canvas, {
+        type: 'line',
+        data: {
+            labels: Object.keys(metrics),
+            datasets: datasets
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                },
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Epoch'
+                    }
+
+                }
+            }
+        }
+    });
+
+}
+
