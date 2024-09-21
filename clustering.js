5ee327ac395310b241622efd33d6bb92d3ca0e06
Alden Deran <aldenderan@protonmail.com>
2024-09-21 12:41:17 -0700

    Start working on cluster editor

--- clustering.js
+++ clustering.js
@@ -1,1 +1,31 @@
 import {NavBar, ServerLoginPane} from "./components.js";
+import {MLServer} from "./MLServer.js";
+
+class ClusterEditor extends HTMLElement {
+    constructor() {
+        super();
+        this.canvas = document.createElement("canvas");
+        this.appendChild(this.canvas);
+
+
+    }
+}
+
+customElements.define("cluster-editor", ClusterEditor);
+
+const server = new MLServer(
+    sessionStorage.getItem('server_url'),  
+    "ai/labels",
+    sessionStorage.getItem('username'), 
+    sessionStorage.getItem('password')
+);
+
+const model_select = document.querySelector('#model-select');
+const model_ids = await server.getModels();
+model_ids.map(
+    (model_id) => {
+        model_select.options.add(new Option(model_id, model_id));
+    }
+);
+
+
