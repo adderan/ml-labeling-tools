6eb92f6fd40393aecf4881d91037a9cb8b769df3
Alden Deran <aldenderan@protonmail.com>
2024-09-23 17:00:11 -0700

    Work on image popup

--- clustering.js
+++ clustering.js
@@ -13,6 +13,42 @@ class Clip {
 
     }
 }
+
+class ClipPreviewBox extends HTMLElement {
+    constructor() {
+        super();
+        this.server = null;
+        this.clip = null;
+        this.x = 0;
+        this.y = 0;
+    }
+
+    connectedCallback() {
+        if (this.clip == null) return;
+        this.dialog = document.createElement("dialog");
+        this.appendChild(this.dialog);
+        this.image_set_label = document.createElement("label");
+        this.image_set_label.innerText = this.clip.image_set;
+        this.dialog.appendChild(this.image_set_label);
+
+        this.classList.add("clip-preview-box");
+        const style = document.createElement("style");
+        style.innerHTML = `
+            .clip-preview-box {
+                dialog {
+                    position: absolute;
+                    left: ${this.x}px;
+                    top: ${this.y}px;
+                }
+            }
+        `;
+        this.appendChild(style);
+        this.dialog.show();
+
+    }
+
+}
+customElements.define("clip-preview-box", ClipPreviewBox);
 class ClusterEditor extends HTMLElement {
     constructor() {
         super();
@@ -24,7 +60,7 @@ class ClusterEditor extends HTMLElement {
         this.canvas.height = this.size;
         this.canvas.width = this.size;
 
-        this.point_size = 8;
+        this.point_size = 4;
 
         this.ctx = this.canvas.getContext("2d");
 
@@ -39,9 +75,32 @@ class ClusterEditor extends HTMLElement {
                 }
             }
         `;
-        this.appendChild(style);
+        //this.appendChild(style);
 
+        this.canvas.addEventListener('click', this);
+        this.preview_box = null;
 
+    }
+    handleEvent(event) {
+        if (event.type == 'click') {
+            if (this.preview_box) {
+                this.preview_box.remove();
+                this.preview_box = null;
+            }
+            const pixel = this.ctx.getImageData(event.offsetX, event.offsetY, 1, 1).data;
+            console.log("Clicked at ", event.offsetX, event.offsetY);
+
+            //if the pixel is (0,0,0), don't display anything
+            if (!pixel.some((p_i) => p_i > 0)) return;
+
+            const clip = this.findNearestAnnotation(event.offsetX, event.offsetY);
+            this.preview_box = document.createElement("clip-preview-box");
+            this.preview_box.server = server;
+            this.preview_box.clip = clip;
+            this.preview_box.x = event.offsetX;
+            this.preview_box.y = event.offsetY;
+            this.appendChild(this.preview_box);
+        }
     }
     setSamples(new_samples) {
         const xvals = new_samples.map((sample) => sample.feature_x);
@@ -59,7 +118,9 @@ class ClusterEditor extends HTMLElement {
         this.color_map = {
             1: "blue",
             2: "red",
-            3: "green"
+            3: "green",
+            4: "yellow",
+            5: "orange"
         };
         this.image_set_color_map = {
             chlorella: "green",
@@ -69,18 +130,67 @@ class ClusterEditor extends HTMLElement {
         };
     }
 
+    encodePixel(x, y) {
+        return `${x} ${y}`;
+    }
+
+    //dfs to find the nearest annotation to pixel (x, y)
+    findNearestAnnotation(start_x, start_y) {
+        
+        const stack = [];
+        const seen = new Set();
+        stack.push([start_x, start_y]);
+        seen.add(this.encodePixel(start_x, start_y));
+        let x, y;
+        while (stack.length > 0) {
+            if (stack.length > 1000) break;
+            const pixel = stack.shift();
+            [x,y] = pixel;
+
+            const xy_str = this.encodePixel(x, y);
+            if (xy_str in this.annotations) {
+                return this.annotations[xy_str];
+            }
+
+            const adj = [[x-1,y], [x+1,y], [x,y-1], [x,y+1]];
+
+            for (let [adj_x, adj_y] of adj) {
+                if ((adj_x < 0) || (adj_x > this.canvas.width) || (adj_y < 0) || (adj_y > this.canvas.height)) {
+                    continue;
+                }
+                const adj_str = this.encodePixel(adj_x, adj_y);
+                if (adj_str in seen) continue;
+                stack.push([adj_x, adj_y]);
+                seen.add(adj_str);
+            }
+
+        }
+        return null;
+
+    }
+
     draw() {
+        this.annotations = {};
         for (let sample of this.samples) {
             this.ctx.fillStyle = this.color_map[sample.class_id];
             //this.ctx.fillStyle = this.image_set_color_map[sample.image_set];
-            this.ctx.fillRect(
-                sample.feature_x * this.size,
-                sample.feature_y * this.size,
+            const center_x = Math.round(sample.feature_x * this.size);
+            const center_y = Math.round(sample.feature_y * this.size);
+            this.ctx.beginPath();
+            this.ctx.arc(
+                center_x,
+                center_y,
                 this.point_size,
-                this.point_size
+                0,
+                Math.PI * 2
             );
+            this.ctx.fill();
+
+            this.annotations[this.encodePixel(center_x, center_y)] = sample;
 
         }
+        //this.image_data = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+        //console.log(this.image_data);
 
     }
     
